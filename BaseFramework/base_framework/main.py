019a66fddf895ed720f80de2aeb267eb7c481411
ArIonMark1 <andreydatsenko777@gmail.com>
2022-01-31 10:03:08 +0200

    lesson_2

--- BaseFramework/base_framework/main.py
+++ BaseFramework/base_framework/main.py
@@ -3,6 +3,7 @@ import sys
 from pprint import pprint
 
 
+# -----------------------------------------
 def parse_input_data(data: str):
     res = {}
     if data:
@@ -16,35 +17,48 @@ def parse_input_data(data: str):
     return res
 
 
+# -----------------------------------------
+
 def control_request(environ):
     request_method = environ['REQUEST_METHOD']
     content_length = environ['CONTENT_LENGTH']
-    pprint(environ)
-    print(content_length)
+    user_data = environ["wsgi.input"]
+
+    print(f'Длина переданых данных: {content_length}')
     # ###############################################################
-    if request_method == 'POST' and int(content_length):
-        data = environ['wsgi.input'].read(int(content_length))
-        print(data)
+    if request_method == 'POST':
+
+        byte_data = environ['wsgi.input'].read(int(content_length))
+        print(byte_data)
 
         path = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(path, 'post_requests\\request.txt')
 
         # ............................................................
         with open(path, 'w', encoding='utf-8') as f:
-            f.write(f' User data: {str(data.decode("utf-8"))}')
+            data_string = byte_data.decode("utf-8")
+            data_string = parse_input_data(data_string)
+            f.write(f' User data: {data_string}')
         # ............................................................
 
         # ###############################################################
 
         print(f'Data in query string: {environ["QUERY_STRING"]}')
-
         request_res = parse_input_data(environ["QUERY_STRING"])
-        user_data = environ["wsgi.input"]
-        print(f'User data in request POST: {user_data}')
-        print(f'Data in query string: {request_res}')
-        return request_method
-    return request_method
 
+        print(f'User data in wsgi_input, request POST: {data_string}')
+        print(f'Data in query string: {request_res}')
+
+        return request_method
+
+    elif request_method == 'GET':
+        print("Пжлст ны дыньые, смтры")
+        return request_method
+    else:
+        return f'Сайт поддерживает только GET и POST запросы!!! Был вызван: {request_method}'
+
+
+# -----------------------------------------
 
 class NotFoundPage:
     def __call__(self, request):
